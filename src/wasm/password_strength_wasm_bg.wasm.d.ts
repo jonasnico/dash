604df672dad1f406f85d748bf2d3b970e040b83a
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-07-10 01:12:52 +0200

    Add Rust WebAssembly build artifacts for deployment

---
+++ src/wasm/password_strength_wasm_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const analyze_password_score: (a: number, b: number) => number;
+export const get_strength_level: (a: number) => [number, number];
+export const calculate_entropy: (a: number, b: number) => number;
+export const benchmark_computation: (a: number, b: number, c: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_start: () => void;
